b8b6fe24f2e3b59426fc4130135697c8d0b35c73
yojik <yoji.kanno@gmail.com>
2022-01-15 06:21:34 +0000

    なんとなく形にする

--- marp.sh
+++ marp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -v $PWD:/home/marp/app/ -e LANG=$LANG -e MARP_USER="$(id -u):$(id -g)"   marpteam/marp-cli:v1.5.0  presentation.md --images png
+docker run --rm -v $PWD:/home/marp/app/ -e LANG=$LANG -e MARP_USER="$(id -u):$(id -g)"   marpteam/marp-cli:v1.5.0  presentation.md --images png -o dist/slide.png
